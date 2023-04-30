f43a81eeab000613e78475665939c1dadd368a32
evargasm19 <evargasm19@gmail.com>
2023-04-30 13:16:33 -0500

    para mis amigos los numeros primos

---
+++ PrimeNumberWithFactors.py
@@ -0,0 +1,21 @@
+'''
+Created on Apr 29, 2023
+
+@author: Edwin
+'''
+x=5
+z=1
+factor=[]
+for y in range(1,x+1) :
+        
+    if x % y == 0 :
+        factor=factor+[y]
+        
+    else : z=z+1
+print('factors of ' + str(x) + ' : ' + str(factor))    
+
+if 2==len(factor) :
+    print(str(x) + ' is a beatiful PRIME number')
+else : 
+    
+    print(str(x) + ' is not a prime number')
